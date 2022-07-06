77052a883a309b20a9232f97e1b37b6f365e9609
xueya1860 <xueya1860@gmail.com>
2022-07-06 10:46:34 +0800

    invisible input to start-edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {startEdit} from './start-edit';
 
-function App() {
+const App = () => {
+  const [focused, setFocus] = React.useState<boolean>(false);
+  const [isEditing, setEditing] = React.useState<boolean>(false);
+  const [value, setValue] = React.useState<string>('');
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const setFocusCb = React.useCallback((event: React.SyntheticEvent<HTMLDivElement>) => {
+    if (isEditing) return;
+    if (!inputRef.current) return;
+
+    event.stopPropagation();
+    setFocus(true);
+    inputRef.current.focus();
+  }, [inputRef, setFocus, isEditing]);
+
+  React.useEffect(() => {
+    const handler = () => {
+      setFocus(false);
+      setEditing(false);
+    };
+    document.addEventListener('click', handler);
+
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  }, [setFocus]);
+
+  React.useEffect(() => {
+    const handler = (event: Event) => {
+      startEdit();
+      setEditing(true);
+    };
+    document.addEventListener('compositionstart', handler);
+    // if (safari && !IPadOrIPhone) || (windows && electron)
+    // Sougou IME will only trigger textInput event
+    document.addEventListener('textInput', handler);
+
+    return () => {
+      document.removeEventListener('compositionstart', handler);
+      document.removeEventListener('textInput', handler);
+    };
+  }, []);
+
+  const onInputChange = React.useCallback((event: any) => {
+    setValue(event.target.value);
+  }, []);
+
+  const inputStyle: React.CSSProperties = {
+    display: isEditing ? 'block' : 'none',
+    position: 'absolute',
+    left: 10,
+    top: 10,
+    bottom: 10,
+    right: 10,
+  };
+
+  const hiddenInputStyle: React.CSSProperties = {
+    position: 'fixed',
+    width: 0,
+    height: 0,
+    left: -1000,
+    top: -1000,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' onClick={setFocusCb} style={focused ? {border: '2px solid crimson', padding: 9} : undefined}>
+      click to focus, then type something
+      {/*TODO: merge onChange with compositionstart*/}
+      <input
+        style={isEditing ? inputStyle : hiddenInputStyle}
+        ref={inputRef}
+        value={value}
+        onChange={onInputChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/start-edit.ts
@@ -0,0 +1,3 @@
+export const startEdit = () => {
+  console.log('start edit');
+};
